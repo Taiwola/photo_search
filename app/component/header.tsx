b71e4f48b7af399fc7c144442a7beeca51e6a2e2
Taiwola <seunolanitori@gmail.com>
2025-01-15 12:41:49 +0100

    refactoring

--- app/component/header.tsx
+++ app/component/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <header>
       <nav className="py-20 px-8 md:px-0 max-w-5xl mx-auto">
-        <ul className="flex justify-between items-center">
+        <ul className="flex flex-col md:flex-row justify-between items-center">
           <li>
             <h5 className="font-bold text-[#9c9ca0] font-pacifico text-2xl">
               PhotoSearch.
